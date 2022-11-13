a556634b0e28c0d92589a811812c866a894f5dfc
WinDanesz <31292708+WinDanesz@users.noreply.github.com>
2022-11-13 22:24:50 +0100

    fix: Fix spectator crash when spectating an entity. Fixes #9

--- src/main/java/com/windanesz/morphspellpack/client/ClientEventHandler.java
+++ src/main/java/com/windanesz/morphspellpack/client/ClientEventHandler.java
@@ -25,13 +25,6 @@ public class ClientEventHandler {
 	public static final ResourceLocation ICONS = new ResourceLocation(MorphSpellPack.MODID, "textures/gui/icons.png");
 	public static int left_height = 39;
 	public static int right_height = 39;
-	
-	@SubscribeEvent
-	public static void RenderGameOverlayEvent(RenderGameOverlayEvent.Pre event) {
-		if (event.getType() == RenderGameOverlayEvent.ElementType.FOOD) {
-			//event.setCanceled(true);
-		}
-	}
 
 	/**
 	 * Lich GUI
@@ -40,73 +33,74 @@ public class ClientEventHandler {
 	public static void RenderGameOverlayEvent(RenderGameOverlayEvent.Post event) {
 		ScaledResolution res = event.getResolution();
 		Minecraft mc = Minecraft.getMinecraft();
-		EntityPlayer player = (EntityPlayer) mc.getRenderViewEntity();
-		if (player != null && player.isSpectator()) {
-			return; // Spectators shouldn't have the spell HUD!
-		}
 
-		if (!LichHandler.isLich(player)) return;
+		if (mc.getRenderViewEntity() instanceof EntityPlayer) {
 
-		GlStateManager.enableBlend();
-
-		int width = res.getScaledWidth();
-		int height = res.getScaledHeight();
-		int left = width / 2 + 91;
-		int top = height - right_height;
-		right_height += 10;
-		boolean unused = false;// Unused flag in vanilla, seems to be part of a 'fade out' mechanic
-
-
-		GlStateManager.pushMatrix();
-
-		Minecraft.getMinecraft().renderEngine.bindTexture(ICONS);
-		FoodStats stats = mc.player.getFoodStats();
-		int level = stats.getFoodLevel();
-		for (int i = 0; i < 10; ++i) {
-			int idx = i * 2 + 1;
-			int x = left - i * 8 - 9;
-			int y = top;
-			int icon = 16;
-			byte background = 0;
-
-			if (mc.player.isPotionActive(MobEffects.HUNGER)) {
-				icon += 36;
-				background = 13;
-			}
-			if (unused) {
-				background = 1; //Probably should be a += 1 but vanilla never uses this
+			EntityPlayer player = (EntityPlayer) mc.getRenderViewEntity();
+			if (player.isSpectator()) {
+				return; // Spectators shouldn't have HUD changes
 			}
 
-//			if (player.getFoodStats().getSaturationLevel() <= 0.0F && updateCounter % (level * 3 + 1) == 0) {
-//				y = top + (player.world.rand.nextInt(3) - 1);
-//			}
+			if (!LichHandler.isLich(player)) { return; }
 
-			drawTexturedModalRect(x, y, 16 + background * 9, 27, 9, 9);
+			GlStateManager.enableBlend();
 
-			if (idx < level) { drawTexturedModalRect(x, y, icon + 36, 27, 9, 9); } else if (idx == level) { drawTexturedModalRect(x, y, icon + 45, 27, 9, 9); }
+			int width = res.getScaledWidth();
+			int height = res.getScaledHeight();
+			int left = width / 2 + 91;
+			int top = height - right_height;
+			right_height += 10;
+			boolean unused = false;// Unused flag in vanilla, seems to be part of a 'fade out' mechanic
+
+			GlStateManager.pushMatrix();
+
+			Minecraft.getMinecraft().renderEngine.bindTexture(ICONS);
+			FoodStats stats = mc.player.getFoodStats();
+			int level = stats.getFoodLevel();
+			for (int i = 0; i < 10; ++i) {
+				int idx = i * 2 + 1;
+				int x = left - i * 8 - 9;
+				int y = top;
+				int icon = 16;
+				byte background = 0;
+
+				if (mc.player.isPotionActive(MobEffects.HUNGER)) {
+					icon += 36;
+					background = 13;
+				}
+				if (unused) {
+					background = 1; //Probably should be a += 1 but vanilla never uses this
+				}
+
+				drawTexturedModalRect(x, y, 16 + background * 9, 27, 9, 9);
+
+				if (idx < level) { drawTexturedModalRect(x, y, icon + 36, 27, 9, 9); } else if (idx == level) {
+					drawTexturedModalRect(x, y, icon + 45, 27, 9, 9);
+				}
+			}
+
+			GlStateManager.popMatrix();
 		}
-
-		GlStateManager.popMatrix();
 
 	}
-	
-	public static void drawTexturedModalRect(int x, int y, int textureX, int textureY, int width, int height)
-	{
+
+	public static void drawTexturedModalRect(int x, int y, int textureX, int textureY, int width, int height) {
 		int zIndex = 100;
 		float f = 0.00390625F;
 		float f1 = 0.00390625F;
 		Tessellator tessellator = Tessellator.getInstance();
 		BufferBuilder bufferbuilder = tessellator.getBuffer();
 		bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX);
-		bufferbuilder.pos((double)(x + 0), (double)(y + height), (double)zIndex).tex((double)((float)(textureX + 0) * 0.00390625F), (double)((float)(textureY + height) * 0.00390625F)).endVertex();
-		bufferbuilder.pos((double)(x + width), (double)(y + height), (double)zIndex).tex((double)((float)(textureX + width) * 0.00390625F), (double)((float)(textureY + height) * 0.00390625F)).endVertex();
-		bufferbuilder.pos((double)(x + width), (double)(y + 0), (double)zIndex).tex((double)((float)(textureX + width) * 0.00390625F), (double)((float)(textureY + 0) * 0.00390625F)).endVertex();
-		bufferbuilder.pos((double)(x + 0), (double)(y + 0), (double)zIndex).tex((double)((float)(textureX + 0) * 0.00390625F), (double)((float)(textureY + 0) * 0.00390625F)).endVertex();
+		bufferbuilder.pos((double) (x + 0), (double) (y + height), (double) zIndex).tex((double) ((float) (textureX + 0) * 0.00390625F), (double) ((float) (textureY + height) * 0.00390625F)).endVertex();
+		bufferbuilder.pos((double) (x + width), (double) (y + height), (double) zIndex).tex((double) ((float) (textureX + width) * 0.00390625F), (double) ((float) (textureY + height) * 0.00390625F)).endVertex();
+		bufferbuilder.pos((double) (x + width), (double) (y + 0), (double) zIndex).tex((double) ((float) (textureX + width) * 0.00390625F), (double) ((float) (textureY + 0) * 0.00390625F)).endVertex();
+		bufferbuilder.pos((double) (x + 0), (double) (y + 0), (double) zIndex).tex((double) ((float) (textureX + 0) * 0.00390625F), (double) ((float) (textureY + 0) * 0.00390625F)).endVertex();
 		tessellator.draw();
 	}
+
 	@SubscribeEvent
-	public static void onRenderLivingEvent(RenderLivingEvent.Pre<?> event){
-		if(event.getEntity().isPotionActive(MSPotions.conjured_soul)){
+	public static void onRenderLivingEvent(RenderLivingEvent.Pre<?> event) {
+		if (event.getEntity().isPotionActive(MSPotions.conjured_soul)) {
 			GlStateManager.pushMatrix();
 			GlStateManager.enableBlend();
 
@@ -116,8 +110,8 @@ public class ClientEventHandler {
 	}
 
 	@SubscribeEvent
-	public static void onRenderLivingEvent(RenderLivingEvent.Post<?> event){
-		if(event.getEntity().isPotionActive(MSPotions.conjured_soul)){
+	public static void onRenderLivingEvent(RenderLivingEvent.Post<?> event) {
+		if (event.getEntity().isPotionActive(MSPotions.conjured_soul)) {
 			GlStateManager.popMatrix();
 		}
 	}
