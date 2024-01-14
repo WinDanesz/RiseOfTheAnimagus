f555a43966f1ecee8a41f8c80a313a50116f0cfd
WinDanesz <31292708+windanesz@users.noreply.github.com>
2024-01-14 23:53:00 +0100

    feat: Pig morphs can find mushrooms, horses, donkeys and mules have speed and jump boost, rabbits can find carrots and have jump boost

---
+++ src/main/java/com/windanesz/morphspellpack/ability/active/AbilityPassivePotion.java
@@ -0,0 +1,75 @@
+package com.windanesz.morphspellpack.ability.active;
+
+import com.windanesz.morphspellpack.ability.IActiveAbility;
+import me.ichun.mods.morph.api.ability.Ability;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AbilityPassivePotion extends Ability {
+
+	public static final String name = "passivePotion";
+	private List<PotionInfo> potions = new ArrayList<>();
+
+	public int cooldown = 0;
+
+	public static class PotionInfo {
+		public String potionName;
+		public int amplifier;
+		public int effectDuration;
+		public Potion potion;
+
+		public PotionInfo(String potionName, int amplifier, int effectDuration) {
+			this.potionName = potionName;
+			this.amplifier = amplifier;
+			this.effectDuration = effectDuration;
+			this.potion = ForgeRegistries.POTIONS.getValue(new ResourceLocation(potionName));
+		}
+	}
+
+	@SuppressWarnings("unused")
+	public AbilityPassivePotion() {
+	}
+
+	public AbilityPassivePotion(List<PotionInfo> potions) {
+		this.potions = potions;
+	}
+
+	@Override
+	public Ability parse(String[] args) {
+		for (int i = 0; i < args.length; i += 3) {
+			String potionName = args[i];
+			int amplifier = Integer.parseInt(args[i + 1]);
+			int effectDuration = Integer.parseInt(args[i + 2]);
+			potions.add(new PotionInfo(potionName, amplifier, effectDuration));
+		}
+		return this;
+	}
+
+	@Override
+	public Ability clone() {
+		return new AbilityPassivePotion(new ArrayList<>(potions));
+	}
+
+	@Override
+	public String getType() {
+		return name;
+	}
+
+	@Override
+	public void tick() {
+		if (getParent() instanceof EntityPlayerMP && getParent().ticksExisted % 20 == 0) {
+			EntityPlayerMP caster = (EntityPlayerMP) getParent();
+			for (PotionInfo potionInfo : potions) {
+				if (potionInfo.potion != null) {
+					caster.addPotionEffect(new PotionEffect(potionInfo.potion, potionInfo.effectDuration, potionInfo.amplifier));
+				}
+			}
+		}
+	}
+}
---
+++ src/main/java/com/windanesz/morphspellpack/ability/active/AbilityPigMushroomSpawn.java
@@ -0,0 +1,13 @@
+package com.windanesz.morphspellpack.ability.active;
+
+import net.minecraft.world.biome.BiomeForest;
+
+public class AbilityPigMushroomSpawn extends AbilityItemSpawn {
+
+	public static final String name = "itemSpawnPigMushroom";
+
+	@Override
+	public boolean conditionPredicate() {
+		return super.conditionPredicate() && this.getParent() != null && this.getParent().world.getBiomeProvider().getBiome(this.getParent().getPosition()) instanceof BiomeForest;
+	}
+}
